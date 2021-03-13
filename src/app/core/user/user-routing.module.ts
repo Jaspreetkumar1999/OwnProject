6ad41c322bb3eb35d3bb75694b658daa3648756f
Karan <duggalkaran400@gmail.com>
2021-03-13 23:31:48 +0530

    login form

--- src/app/core/user/user-routing.module.ts
+++ src/app/core/user/user-routing.module.ts
@@ -1,11 +1,16 @@
 import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 const routes: Routes = [
   {
     path :"",
     component : UserComponent
+  },
+  {
+    path :"login",
+    component : LoginSignupComponent
   }
 ];
 
