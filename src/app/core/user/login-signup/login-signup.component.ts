3841bce01491f8f9ca4f9fcd382dfdf20e32823d
Karan <duggalkaran400@gmail.com>
2021-03-14 10:15:23 +0530

    login

--- src/app/core/user/login-signup/login-signup.component.ts
+++ src/app/core/user/login-signup/login-signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder , FormControl, FormGroup, FormGroupName, Validators} from "@angular/forms"
-
+import {ApiServiceService} from 'src/app/_services/api-service.service'
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
@@ -10,16 +10,18 @@ export class LoginSignupComponent implements OnInit {
 
   loginForm:any
   constructor(
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder,
+    private apiServiceService:ApiServiceService
   ) {
     this.loginForm = formBuilder.group({
       email: new FormControl(null,{validators:[Validators.required,Validators.email]}),
       password: new FormControl(null,{validators:[Validators.required]}),
     })
   }
-
+  
   logindata(){
     console.log(this.loginForm.value);
+    // this.apiServiceService.callAPI('post')
     
   }
 
