1b0fdd62bed90d161564f641833779dcf059c26a
Karan <duggalkaran400@gmail.com>
2021-03-14 17:17:30 +0530

    eror

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiKey :"hello123",
-  baseUrl : "http://52.90.163.87:3001/api/"
+  baseUrl : "http://52.90.163.87:3001/api/",
+  postmanApiLink: 'https://www.getpostman.com/collections/445431c04440bfe24f8e'
 };
